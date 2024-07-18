3d405e9d67061e14f552c81fe04bdc37d89e3d5f
aramalipoor <aram.alipoor@gmail.com>
2024-07-18 19:08:08 +0200

    docs: add complete erpc.yaml config example

---
+++ docs/pages/config/_meta.js
@@ -0,0 +1,20 @@
+module.exports = {
+	"complete-example": {
+		title: "erpc.yml",
+	},
+	upstreams: {
+		title: "Upstreams",
+	},
+	failsafe: {
+		title: "Failsafe",
+	},
+	database: {
+		title: "Database (Caching)",
+	},
+	"rate-limiters": {
+		title: "Rate limiters",
+	},
+	"health-checks": {
+		title: "Health checks",
+	},
+};
