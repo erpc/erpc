01304ecdc5a50dccc2954299402750b7b85e9b2e
aramalipoor <aram.alipoor@gmail.com>
2024-07-18 17:12:00 +0200

    docs: add basic structure

---
+++ docs/pages/_meta.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "index": {
+        "title": "Quick start"
+    },
+    "why": { title: "Why eRPC?" },
+    "-- Config": {
+      "type": "separator",
+      "title": "Config"
+    },
+    "config": { display: "children", title: "Config" },
+    "-- Deployment": {
+      "type": "separator",
+      "title": "Deployment"
+    },
+    "deployment": { display: "children", title: "Deployment" },
+    "-- Operation": {
+      "type": "separator",
+      "title": "Operation"
+    },
+    "operation": { display: "children", title: "Operation" },
+}
