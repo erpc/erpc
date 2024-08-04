92bee3b3c48d9592d50246a2a2817bb8ff291541
aram.eth <aram.alipoor@gmail.com>
2024-08-05 00:02:50 +0200

    feat: add cors config per project (#30)

--- erpc/http_server.go
+++ erpc/http_server.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/erpc/erpc/auth"
 	"github.com/erpc/erpc/common"
+	"github.com/erpc/erpc/health"
 	"github.com/erpc/erpc/upstream"
 	"github.com/rs/zerolog"
 )
@@ -19,6 +20,8 @@ import (
 type HttpServer struct {
 	config *common.ServerConfig
 	server *http.Server
+	erpc   *ERPC
+	logger *zerolog.Logger
 }
 
 func NewHttpServer(ctx context.Context, logger *zerolog.Logger, cfg *common.ServerConfig, erpc *ERPC) *HttpServer {
@@ -32,93 +35,18 @@ func NewHttpServer(ctx context.Context, logger *zerolog.Logger, cfg *common.Serv
 		timeOutDur = 30 * time.Second
 	}
 
-	handler := http.NewServeMux()
-	handler.HandleFunc("/", func(hrw http.ResponseWriter, r *http.Request) {
-		requestCtx, cancel := context.WithTimeout(r.Context(), timeOutDur)
-		defer cancel()
-
-		resultChan := make(chan common.NormalizedResponse, 1)
-		errChan := make(chan error, 1)
-
-		go func() {
-			logger.Debug().Msgf("received request on path: %s with body length: %d", r.URL.Path, r.ContentLength)
-
-			segments := strings.Split(r.URL.Path, "/")
-			// Check if the URL path has at least three segments ("/main/evm/1")
-			if len(segments) != 4 {
-				errChan <- common.NewErrInvalidUrlPath(r.URL.Path)
-				return
-			}
-
-			projectId := segments[1]
-			networkId := fmt.Sprintf("%s:%s", segments[2], segments[3])
-
-			body, err := io.ReadAll(r.Body)
-			if err != nil {
-				logger.Error().Err(err).Msgf("failed to read request body")
-				errChan <- common.NewErrInvalidRequest(err)
-				return
-			}
-
-			logger.Debug().Msgf("received request for projectId: %s, networkId: %s with body: %s", projectId, networkId, body)
-
-			project, err := erpc.GetProject(projectId)
-			if err != nil {
-				logger.Error().Err(err).Msgf("failed to get project %s", projectId)
-				errChan <- err
-				return
-			}
-
-			nw, err := erpc.GetNetwork(projectId, networkId)
-			if err != nil {
-				logger.Error().Err(err).Msgf("failed to get network %s for project %s", networkId, projectId)
-				errChan <- err
-				return
-			}
-
-			nq := upstream.NewNormalizedRequest(body)
-			nq.SetNetwork(nw)
-			nq.ApplyDirectivesFromHttpHeaders(r.Header)
-
-			ap, err := auth.NewPayloadFromHttp(project.Config, nq, r)
-			if err != nil {
-				logger.Error().Err(err).Str("projectId", projectId).Msgf("failed to parse authentication payload")
-				errChan <- err
-				return
-			}
-			if err := project.Authenticate(requestCtx, nq, ap); err != nil {
-				logger.Error().Err(err).Str("projectId", projectId).Msgf("unauthorized request rejected")
-				errChan <- err
-				return
-			}
-
-			resp, err := project.Forward(requestCtx, networkId, nq)
-			if err != nil {
-				errChan <- err
-				return
-			}
-			logger.Debug().Msgf("request forwarded successfully for projectId: %s, networkId: %s", projectId, networkId)
-			resultChan <- resp
-		}()
-
-		select {
-		case resp := <-resultChan:
-			hrw.Header().Set("Content-Type", "application/json")
-			hrw.WriteHeader(http.StatusOK)
-			hrw.Write(resp.Body())
-		case err := <-errChan:
-			handleErrorResponse(logger, err, hrw)
-		case <-requestCtx.Done():
-			handleErrorResponse(logger, common.NewErrRequestTimeOut(timeOutDur), hrw)
-		}
-	})
-
 	srv := &HttpServer{
 		config: cfg,
-		server: &http.Server{
-			Addr:    addr,
-			Handler: handler,
-		},
+		erpc:   erpc,
+		logger: logger,
+	}
+
+	handler := http.NewServeMux()
+	handler.HandleFunc("/", srv.handleRequest(timeOutDur))
+
+	srv.server = &http.Server{
+		Addr:    addr,
+		Handler: handler,
 	}
 
 	go func() {
@@ -134,6 +62,148 @@ func NewHttpServer(ctx context.Context, logger *zerolog.Logger, cfg *common.Serv
 	}()
 
 	return srv
+}
+
+func (s *HttpServer) handleRequest(timeOutDur time.Duration) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		segments := strings.Split(r.URL.Path, "/")
+		if len(segments) != 4 {
+			handleErrorResponse(s.logger, common.NewErrInvalidUrlPath(r.URL.Path), w)
+			return
+		}
+
+		projectId := segments[1]
+		networkId := fmt.Sprintf("%s:%s", segments[2], segments[3])
+
+		project, err := s.erpc.GetProject(projectId)
+		if err != nil {
+			handleErrorResponse(s.logger, err, w)
+			return
+		}
+
+		// Apply CORS if configured
+		if project.Config.CORS != nil {
+			if !s.handleCORS(w, r, project.Config.CORS) {
+				return
+			}
+
+			if r.Method == http.MethodOptions {
+				return // CORS preflight request handled
+			}
+		}
+
+		requestCtx, cancel := context.WithTimeout(r.Context(), timeOutDur)
+		defer cancel()
+
+		resultChan := make(chan common.NormalizedResponse, 1)
+		errChan := make(chan error, 1)
+
+		go func() {
+			body, err := io.ReadAll(r.Body)
+			if err != nil {
+				errChan <- common.NewErrInvalidRequest(err)
+				return
+			}
+
+			s.logger.Debug().Msgf("received request for projectId: %s, networkId: %s with body: %s", projectId, networkId, body)
+
+			nw, err := s.erpc.GetNetwork(projectId, networkId)
+			if err != nil {
+				errChan <- err
+				return
+			}
+
+			nq := upstream.NewNormalizedRequest(body)
+			nq.SetNetwork(nw)
+			nq.ApplyDirectivesFromHttpHeaders(r.Header)
+
+			ap, err := auth.NewPayloadFromHttp(project.Config, nq, r)
+			if err != nil {
+				errChan <- err
+				return
+			}
+
+			if err := project.Authenticate(requestCtx, nq, ap); err != nil {
+				errChan <- err
+				return
+			}
+
+			resp, err := project.Forward(requestCtx, networkId, nq)
+			if err != nil {
+				errChan <- err
+				return
+			}
+
+			s.logger.Debug().Msgf("request forwarded successfully for projectId: %s, networkId: %s", projectId, networkId)
+			resultChan <- resp
+		}()
+
+		select {
+		case resp := <-resultChan:
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			w.Write(resp.Body())
+		case err := <-errChan:
+			handleErrorResponse(s.logger, err, w)
+		case <-requestCtx.Done():
+			handleErrorResponse(s.logger, common.NewErrRequestTimeOut(timeOutDur), w)
+		}
+	}
+}
+func (s *HttpServer) handleCORS(w http.ResponseWriter, r *http.Request, corsConfig *common.CORSConfig) bool {
+	origin := r.Header.Get("Origin")
+	if origin == "" {
+		return true // Not a CORS request
+	}
+
+	// Count all CORS requests
+	health.MetricCORSRequestsTotal.WithLabelValues(r.URL.Path, origin).Inc()
+
+	// Check if the origin is allowed
+	allowed := false
+	for _, allowedOrigin := range corsConfig.AllowedOrigins {
+		if common.WildcardMatch(allowedOrigin, origin) {
+			allowed = true
+			break
+		}
+	}
+
+	if !allowed {
+		s.logger.Debug().Str("origin", origin).Msg("CORS request from disallowed origin")
+		health.MetricCORSDisallowedOriginTotal.WithLabelValues(r.URL.Path, origin).Inc()
+		
+		if r.Method == http.MethodOptions {
+			// For preflight requests, return 204 No Content
+			w.WriteHeader(http.StatusNoContent)
+		} else {
+			// For actual requests, return 403 Forbidden
+			http.Error(w, "CORS request from disallowed origin", http.StatusForbidden)
+		}
+		return false
+	}
+
+	// Set CORS headers
+	w.Header().Set("Access-Control-Allow-Origin", origin)
+	w.Header().Set("Access-Control-Allow-Methods", strings.Join(corsConfig.AllowedMethods, ", "))
+	w.Header().Set("Access-Control-Allow-Headers", strings.Join(corsConfig.AllowedHeaders, ", "))
+	w.Header().Set("Access-Control-Expose-Headers", strings.Join(corsConfig.ExposedHeaders, ", "))
+
+	if corsConfig.AllowCredentials {
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+	}
+
+	if corsConfig.MaxAge > 0 {
+		w.Header().Set("Access-Control-Max-Age", fmt.Sprintf("%d", corsConfig.MaxAge))
+	}
+
+	// Handle preflight request
+	if r.Method == http.MethodOptions {
+		health.MetricCORSPreflightRequestsTotal.WithLabelValues(r.URL.Path, origin).Inc()
+		w.WriteHeader(http.StatusNoContent)
+		return false
+	}
+
+	return true
 }
 
 func handleErrorResponse(logger *zerolog.Logger, err error, hrw http.ResponseWriter) {
