ea918ecf0afebccd2316e726370e0c4a747674c8
kasrakhosravi <kasrakhosravi2@gmail.com>
2024-08-22 17:33:29 +0200

    fix: fetch chainId and check it against thirdweb upstream req for validation

--- upstream/thirdweb_http_json_rpc_client.go
+++ upstream/thirdweb_http_json_rpc_client.go
@@ -3,141 +3,20 @@ package upstream
 import (
 	"context"
 	"fmt"
+	"math/rand"
 	"net/url"
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/erpc/erpc/common"
 )
 
-var thirdwebNetworkSubdomains = map[int64]struct{}{
-	1:           {}, // Ethereum Mainnet
-	10:          {}, // OP Mainnet
-	56:          {}, // BNB Smart Chain Mainnet
-	100:         {}, // Gnosis
-	137:         {}, // Polygon Mainnet
-	204:         {}, // opBNB Mainnet
-	252:         {}, // Fraxtal
-	324:         {}, // zkSync Mainnet
-	5000:        {}, // Mantle
-	7560:        {}, // Cyber Mainnet
-	8333:        {}, // B3
-	8453:        {}, // Base
-	34443:       {}, // Mode
-	42026:       {}, // Donatuz
-	42161:       {}, // Arbitrum One
-	42170:       {}, // Arbitrum Nova
-	43114:       {}, // Avalanche C-Chain
-	59144:       {}, // Linea
-	84532:       {}, // Base Sepolia Testnet
-	421614:      {}, // Arbitrum Sepolia
-	534352:      {}, // Scroll
-	660279:      {}, // Xai Mainnet
-	7777777:     {}, // Zora
-	11155111:    {}, // Sepolia
-	11155420:    {}, // OP Sepolia Testnet
-	666666666:   {}, // Degen Chain
-	888888888:   {}, // Ancient8
-	30:          {}, // Rootstock Mainnet
-	31:          {}, // Rootstock Testnet
-	47:          {}, // Xpla Testnet
-	97:          {}, // BNB Smart Chain Testnet
-	122:         {}, // Fuse Mainnet
-	123:         {}, // Fuse Sparknet
-	250:         {}, // Fantom Opera
-	288:         {}, // Boba Network
-	300:         {}, // zkSync Sepolia Testnet
-	302:         {}, // zkCandy Sepolia Testnet
-	335:         {}, // DFK Chain Test
-	690:         {}, // Redstone
-	919:         {}, // Mode Testnet
-	957:         {}, // Lyra Chain
-	1001:        {}, // Kaia Testnet Kairos
-	1088:        {}, // Metis Andromeda Mainnet
-	1101:        {}, // Polygon zkEVM
-	1135:        {}, // Lisk
-	1284:        {}, // Moonbeam
-	1993:        {}, // B3 Sepolia Testnet
-	2039:        {}, // Aleph Zero Testnet
-	2040:        {}, // Vanar Mainnet
-	2442:        {}, // Polygon zkEVM Cardona Testnet
-	2522:        {}, // Fraxtal Testnet
-	2662:        {}, // APEX
-	4202:        {}, // Lisk Sepolia Testnet
-	5003:        {}, // Mantle Sepolia Testnet
-	7887:        {}, // Kinto Mainnet
-	8217:        {}, // Kaia Mainnet
-	10200:       {}, // Gnosis Chiado Testnet
-	11124:       {}, // Abstract Testnet
-	17069:       {}, // Garnet Holesky
-	22222:       {}, // Nautilus Mainnet
-	41455:       {}, // Aleph Zero EVM
-	42220:       {}, // Celo Mainnet
-	43113:       {}, // Avalanche Fuji Testnet
-	44787:       {}, // Celo Dango Testnet
-	53935:       {}, // DFK Chain
-	59141:       {}, // Linea Sepolia
-	60808:       {}, // BOB
-	78600:       {}, // Vanguard
-	80002:       {}, // Amoy
-	81457:       {}, // Blast
-	98985:       {}, // Superposition Testnet
-	132902:      {}, // Form Testnet
-	167000:      {}, // Taiko Mainnet
-	167009:      {}, // Taiko Hekla L2
-	325000:      {}, // Camp Network Testnet V2
-	534351:      {}, // Scroll Sepolia Testnet
-	978657:      {}, // Treasure Ruby
-	28122024:    {}, // Ancient8 Testnet
-	37084624:    {}, // SKALE Nebula Hub Testnet
-	111557560:   {}, // Cyber Testnet
-	161221135:   {}, // Plume Testnet
-	168587773:   {}, // Blast Sepolia Testnet
-	994873017:   {}, // Lumia Prism
-	999999999:   {}, // Zora Sepolia Testnet
-	1380012617:  {}, // RARI Chain Mainnet
-	1903648807:  {}, // Gemuchain Testnet
-	1952959480:  {}, // Lumia Testnet
-	37714555429: {}, // Xai Testnet v2
-	2:           {}, // Expanse Network
-	7:           {}, // ThaiChain
-	8:           {}, // Ubiq
-	9:           {}, // Ubiq Network Testnet
-	11:          {}, // Metadium Mainnet
-	12:          {}, // Metadium Testnet
-	13:          {}, // Diode Testnet Staging
-	14:          {}, // Flare Mainnet
-	15:          {}, // Diode Prenet
-	16:          {}, // Songbird Testnet Coston
-	17:          {}, // ThaiChain 2.0 ThaiFi
-	18:          {}, // ThunderCore Testnet
-	19:          {}, // Songbird Canary-Network
-	20:          {}, // Elastos Smart Chain
-	21:          {}, // Elastos Smart Chain Testnet
-	22:          {}, // ELA-DID-Sidechain Mainnet
-	23:          {}, // ELA-DID-Sidechain Testnet
-	24:          {}, // KardiaChain Mainnet
-	25:          {}, // Cronos Mainnet
-	26:          {}, // Genesis L1 testnet
-	27:          {}, // ShibaChain
-	29:          {}, // Genesis L1
-	32:          {}, // GoodData Testnet
-	33:          {}, // GoodData Mainnet
-	34:          {}, // SecureChain Mainnet
-	35:          {}, // TBWG Chain
-	36:          {}, // Dxchain Mainnet
-	37:          {}, // Xpla Mainnet
-	38:          {}, // Valorbit
-	39:          {}, // U2U Solaris Mainnet
-	40:          {}, // Telos EVM Mainnet
-	41:          {}, // Telos EVM Testnet
-}
-
 type ThirdwebHttpJsonRpcClient struct {
 	upstream *Upstream
 	clientId string
-	clients  map[string]HttpJsonRpcClient
+	clients  map[int64]HttpJsonRpcClient
 	mu       sync.RWMutex
 }
 
@@ -154,7 +33,7 @@ func NewThirdwebHttpJsonRpcClient(pu *Upstream, parsedUrl *url.URL) (HttpJsonRpc
 	return &ThirdwebHttpJsonRpcClient{
 		upstream: pu,
 		clientId: clientId,
-		clients:  make(map[string]HttpJsonRpcClient),
+		clients:  make(map[int64]HttpJsonRpcClient),
 	}, nil
 }
 
@@ -172,14 +51,55 @@ func (c *ThirdwebHttpJsonRpcClient) SupportsNetwork(networkId string) (bool, err
 		return false, err
 	}
 
-	_, ok := thirdwebNetworkSubdomains[chainId]
-	return ok, nil
+	// Check against endpoint to see if eth_chainId responds successfully
+	client, err := c.createClient(chainId)
+	if err != nil {
+		return false, err
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	rid := rand.Intn(1000000)
+	pr := NewNormalizedRequest([]byte(fmt.Sprintf(`{"jsonrpc":"2.0","id":%d,"method":"eth_chainId","params":[]}`, rid)))
+	resp, err := client.SendRequest(ctx, pr)
+	if err != nil {
+		return false, err
+	}
+
+	jrr, err := resp.JsonRpcResponse()
+	if err != nil {
+		return false, err
+	}
+
+	cidh, err := common.NormalizeHex(jrr.Result)
+	if err != nil {
+		return false, err
+	}
+
+	cid, err := common.HexToInt64(cidh)
+	if err != nil {
+		return false, err
+	}
+
+	return cid == chainId, nil
 }
 
 func (c *ThirdwebHttpJsonRpcClient) getOrCreateClient(network common.Network) (HttpJsonRpcClient, error) {
-	networkID := network.Id()
+	if network.Architecture() != common.ArchitectureEvm {
+		return nil, fmt.Errorf("unsupported network architecture for Thirdweb client: %s", network.Architecture())
+	}
+
+	chainID, err := network.EvmChainId()
+	if err != nil {
+		return nil, err
+	}
+
+	return c.createClient(chainID)
+}
+
+func (c *ThirdwebHttpJsonRpcClient) createClient(chainID int64) (HttpJsonRpcClient, error) {
 	c.mu.RLock()
-	client, exists := c.clients[networkID]
+	client, exists := c.clients[chainID]
 	c.mu.RUnlock()
 
 	if exists {
@@ -190,25 +110,12 @@ func (c *ThirdwebHttpJsonRpcClient) getOrCreateClient(network common.Network) (H
 	defer c.mu.Unlock()
 
 	// Double-check to ensure another goroutine hasn't created the client
-	if client, exists := c.clients[networkID]; exists {
+	if client, exists := c.clients[chainID]; exists {
 		return client, nil
 	}
 
-	if network.Architecture() != common.ArchitectureEvm {
-		return nil, fmt.Errorf("unsupported network architecture for Thirdweb client: %s", network.Architecture())
-	}
-
-	chainID, err := network.EvmChainId()
-	if err != nil {
-		return nil, err
-	}
-
-	if _, ok := thirdwebNetworkSubdomains[chainID]; !ok {
-		return nil, fmt.Errorf("unsupported network chain ID for Thirdweb: %d", chainID)
-	}
-
-	thirdwebURL := fmt.Sprintf("https://%d.rpc.thirdweb.com/%s", chainID, c.clientId)
-	parsedURL, err := url.Parse(thirdwebURL)
+	thirdwebUrl := fmt.Sprintf("https://%d.rpc.thirdweb.com/%s", chainID, c.clientId)
+	parsedURL, err := url.Parse(thirdwebUrl)
 	if err != nil {
 		return nil, err
 	}
@@ -218,7 +125,7 @@ func (c *ThirdwebHttpJsonRpcClient) getOrCreateClient(network common.Network) (H
 		return nil, err
 	}
 
-	c.clients[networkID] = client
+	c.clients[chainID] = client
 	return client, nil
 }
 
