76ce3225c82297ca10f7b9ac8c8de0cb0c7cfd11
kasrakhosravi <kasrakhosravi2@gmail.com>
2024-11-19 16:35:19 +0100

    feat: add og image generation for docs

---
+++ docs/app/og/route.tsx
@@ -0,0 +1,60 @@
+import { ImageResponse } from 'next/og'
+
+export const runtime = 'edge'
+
+const font = fetch(new URL('./Inter-SemiBold.otf', import.meta.url)).then(res =>
+  res.arrayBuffer()
+)
+
+export async function GET(req: Request): Promise<Response> {
+  try {
+    const { searchParams } = new URL(req.url)
+
+    // ?title=<title>
+    const title =
+      searchParams.get('title')?.slice(0, 75) || 'eRPC Docs'
+
+    return new ImageResponse(
+      (
+        <div
+          tw="text-white px-20 py-[70px] bg-[#030303] h-full w-full flex justify-between flex-col"
+          style={{
+            backgroundImage:
+              'radial-gradient(circle at 25px 25px, #333 2%, transparent 0%), radial-gradient(circle at 75px 75px, #333 2%, transparent 0%)',
+            backgroundSize: '100px 100px',
+            backgroundPosition: '-30px -10px'
+          }}
+        >
+          <img height="40" src={`../../assets/logo.jpeg`} />
+          <h1
+            tw="text-transparent text-[82px] m-0 mb-10 tracking-tighter leading-[1.1]"
+            style={{
+              textShadow: '0 2px 30px #000',
+              backgroundImage: 'linear-gradient(90deg, #fff 40%, #aaa)',
+              backgroundClip: 'text'
+            }}
+          >
+            {title}
+          </h1>
+          <p tw="m-0 text-3xl tracking-tight">
+          open-source fault-tolerant evm rpc proxy and cache
+          </p>
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+        fonts: [
+          {
+            name: 'inter',
+            data: await font,
+            style: 'normal'
+          }
+        ]
+      }
+    )
+  } catch (error) {
+    console.error(error)
+    return new Response('Failed to generate the image', { status: 500 })
+  }
+}
