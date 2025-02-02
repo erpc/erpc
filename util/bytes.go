f95807464a77abf5b2480719f6b2c51ed9757f2c
aramalipoor <aram.alipoor@gmail.com>
2025-02-02 20:40:05 +0100

    fix: correct handling of splitted getLogs empty responses

--- util/bytes.go
+++ util/bytes.go
@@ -4,4 +4,21 @@ import "io"
 
 type ByteWriter interface {
 	WriteTo(w io.Writer) (n int64, err error)
+	IsResultEmptyish() bool
+}
+
+func IsBytesEmptyish(b []byte) bool {
+	lnr := len(b)
+	if lnr > 4 {
+		return false
+	}
+	if lnr == 0 ||
+		(lnr == 4 && b[0] == '"' && b[1] == '0' && b[2] == 'x' && b[3] == '"') ||
+		(lnr == 4 && b[0] == 'n' && b[1] == 'u' && b[2] == 'l' && b[3] == 'l') ||
+		(lnr == 2 && b[0] == '"' && b[1] == '"') ||
+		(lnr == 2 && b[0] == '[' && b[1] == ']') ||
+		(lnr == 2 && b[0] == '{' && b[1] == '}') {
+		return true
+	}
+	return false
 }
