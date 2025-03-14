72898eec4124b9f5ee3e68f9e7019ecc8f3932a3
kasrakhosravi <kasrakhosravi2@gmail.com>
2025-03-14 13:46:41 +0100

    fix: remove unnecessary blockTag assignment in JSON-RPC error extraction

--- architecture/evm/error_normalizer.go
+++ architecture/evm/error_normalizer.go
@@ -169,9 +169,6 @@ func ExtractJsonRpcError(r *http.Response, nr *common.NormalizedResponse, jr *co
 			strings.HasPrefix(msg, "Safe block not found") ||
 			strings.HasPrefix(msg, "finalized block not found") ||
 			strings.HasPrefix(msg, "Finalized block not found") {
-
-			details["blockTag"] = strings.ToLower(strings.SplitN(err.Message, " ", 2)[0])
-
 			// by default, we retry this type of clien-side exception as other upstreams might
 			// have/support this specific block tag data.
 			return common.NewErrEndpointClientSideException(
