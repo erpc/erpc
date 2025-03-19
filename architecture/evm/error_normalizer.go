a2e24c6ae4f2d55ce6298fdf377f5bdd82dc531f
kasrakhosravi <kasrakhosravi2@gmail.com>
2025-03-19 10:33:05 +0100

    fix: set JsonRpcErrorUnsupportedException for tx of type error

--- architecture/evm/error_normalizer.go
+++ architecture/evm/error_normalizer.go
@@ -340,7 +340,7 @@ func ExtractJsonRpcError(r *http.Response, nr *common.NormalizedResponse, jr *co
 			return common.NewErrEndpointClientSideException(
 				common.NewErrJsonRpcExceptionInternal(
 					int(code),
-					common.JsonRpcErrorInvalidArgument,
+					common.JsonRpcErrorUnsupportedException,
 					err.Message,
 					nil,
 					details,
@@ -354,7 +354,7 @@ func ExtractJsonRpcError(r *http.Response, nr *common.NormalizedResponse, jr *co
 						return common.NewErrEndpointClientSideException(
 							common.NewErrJsonRpcExceptionInternal(
 								int(code),
-								common.JsonRpcErrorUnsupportedException,
+								common.JsonRpcErrorInvalidArgument,
 								err.Message,
 								nil,
 								details,
