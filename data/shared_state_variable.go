344347b03611b25e090e2346d6719c71491113d5
kasrakhosravi <kasrakhosravi2@gmail.com>
2025-04-16 12:34:15 +0200

    fix: use maybeUpdateValue for fallback remote vals

--- data/shared_state_variable.go
+++ data/shared_state_variable.go
@@ -90,8 +90,8 @@ func (c *counterInt64) TryUpdate(ctx context.Context, newValue int64) int64 {
 		c.mu.Lock()
 		defer c.mu.Unlock()
 
-		currentVal := c.value.Load()
-		_ = c.maybeUpdateValue(currentVal, newValue)
+		currentValue := c.value.Load()
+		_ = c.maybeUpdateValue(currentValue, newValue)
 		return c.value.Load()
 	}
 	defer func() {
@@ -109,8 +109,8 @@ func (c *counterInt64) TryUpdate(ctx context.Context, newValue int64) int64 {
 		c.mu.Lock()
 		defer c.mu.Unlock()
 
-		currentVal := c.value.Load()
-		_ = c.maybeUpdateValue(currentVal, newValue)
+		currentValue := c.value.Load()
+		_ = c.maybeUpdateValue(currentValue, newValue)
 		return c.value.Load()
 	}
 
@@ -121,8 +121,8 @@ func (c *counterInt64) TryUpdate(ctx context.Context, newValue int64) int64 {
 			c.mu.Lock()
 			defer c.mu.Unlock()
 
-			currentVal := c.value.Load()
-			_ = c.maybeUpdateValue(currentVal, newValue)
+			currentValue := c.value.Load()
+			_ = c.maybeUpdateValue(currentValue, remoteValue)
 			return c.value.Load()
 		}
 	}
@@ -132,12 +132,17 @@ func (c *counterInt64) TryUpdate(ctx context.Context, newValue int64) int64 {
 
 	// Use highest value among local, remote, and new
 	currentValue := c.value.Load()
-
-	if remoteValue > currentValue {
-		c.setValue(remoteValue)
+	value := currentValue
+	if remoteValue > value {
+		value = remoteValue
 	}
+	if newValue > value || value > newValue && (value-newValue > c.ignoreDownDriftOf) {
+		value = newValue
 
-	if c.maybeUpdateValue(currentValue, newValue) {
+		if c.driftCallback != nil {
+			c.driftCallback(currentValue, newValue)
+		}
+
 		go func() {
 			// Only update remote if we're using the new value
 			setCtx, setCancel := context.WithCancel(c.registry.appCtx)
@@ -154,6 +159,10 @@ func (c *counterInt64) TryUpdate(ctx context.Context, newValue int64) int64 {
 					Msg("failed to update remote value")
 			}
 		}()
+	}
+
+	if value > 0 {
+		c.setValue(value)
 	}
 
 	return c.value.Load()
@@ -192,8 +201,17 @@ func (c *counterInt64) TryUpdateIfStale(ctx context.Context, staleness time.Dura
 		if err != nil {
 			return c.value.Load(), err
 		}
-		currentVal := c.value.Load()
-		_ = c.maybeUpdateValue(currentVal, newValue)
+
+		currentValue := c.value.Load()
+		if newValue > currentValue {
+			c.setValue(newValue)
+		} else if currentValue > newValue && (currentValue-newValue > c.ignoreDownDriftOf) {
+			c.setValue(newValue)
+
+			if c.driftCallback != nil {
+				c.driftCallback(currentValue, newValue)
+			}
+		}
 		return c.value.Load(), nil
 	}
 	defer func() {
@@ -219,8 +237,17 @@ func (c *counterInt64) TryUpdateIfStale(ctx context.Context, staleness time.Dura
 		if err != nil {
 			return c.value.Load(), err
 		}
-		currentVal := c.value.Load()
-		_ = c.maybeUpdateValue(currentVal, newValue)
+
+		currentValue := c.value.Load()
+		if newValue > currentValue {
+			c.setValue(newValue)
+		} else if currentValue > newValue && (currentValue-newValue > c.ignoreDownDriftOf) {
+			c.setValue(newValue)
+
+			if c.driftCallback != nil {
+				c.driftCallback(currentValue, newValue)
+			}
+		}
 		return c.value.Load(), nil
 	}
 
@@ -251,8 +278,15 @@ func (c *counterInt64) TryUpdateIfStale(ctx context.Context, staleness time.Dura
 		return c.value.Load(), err
 	}
 
-	if c.maybeUpdateValue(currentValue, newValue) {
-		// Only publish if updated
+	// Update if new value is higher
+	currentValue = c.value.Load() // Re-read in case it changed
+	if newValue > currentValue || currentValue > newValue && (currentValue-newValue > c.ignoreDownDriftOf) {
+		c.setValue(newValue)
+
+		if c.driftCallback != nil {
+			c.driftCallback(currentValue, newValue)
+		}
+
 		go func() {
 			err := c.registry.connector.Set(c.registry.appCtx, c.key, "value", fmt.Sprintf("%d", newValue), nil)
 			if err == nil {
