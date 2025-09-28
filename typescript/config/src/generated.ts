7bcdd677399a6b24784cc40f4791eb0aeab8d5ad
aramalipoor <1164589+aramalipoor@users.noreply.github.com>
2025-09-28 08:02:06 +0000

    chore: release 0.0.57

--- typescript/config/src/generated.ts
+++ typescript/config/src/generated.ts
@@ -88,6 +88,7 @@ export interface HealthCheckConfig {
 }
 export type HealthCheckMode = string;
 export const HealthCheckModeSimple: HealthCheckMode = "simple";
+export const HealthCheckModeNetworks: HealthCheckMode = "networks";
 export const HealthCheckModeVerbose: HealthCheckMode = "verbose";
 export const EvalAnyInitializedUpstreams = "any:initializedUpstreams";
 export const EvalAnyErrorRateBelow90 = "any:errorRateBelow90";
@@ -265,12 +266,6 @@ export interface NetworkDefaults {
   directiveDefaults?: DirectiveDefaultsConfig;
   evm?: TsEvmNetworkConfigForDefaults;
 }
-/**
- * Define a type alias to avoid recursion
- */
-/**
- * If that fails, try the old format with single failsafe object
- */
 export interface CORSConfig {
   allowedOrigins: string[];
   allowedMethods: string[];
@@ -306,12 +301,6 @@ export interface UpstreamConfig {
   routing?: RoutingConfig;
   shadow?: ShadowUpstreamConfig;
 }
-/**
- * Define a type alias to avoid recursion
- */
-/**
- * If that fails, try the old format with single failsafe object
- */
 export interface ShadowUpstreamConfig {
   enabled: boolean;
   ignoreFields?: { [key: string]: string[]};
@@ -467,12 +456,6 @@ export interface NetworkConfig {
   alias?: string;
   methods?: MethodsConfig;
 }
-/**
- * Define a type alias to avoid recursion
- */
-/**
- * If that fails, try the old format with single failsafe object
- */
 export interface DirectiveDefaultsConfig {
   retryEmpty?: boolean;
   retryPending?: boolean;
